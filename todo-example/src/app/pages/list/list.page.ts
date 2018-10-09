a2b07791e6bd86eed6cebd4aa8949fc51c8673d7
Brett Koenig <bk@Bretts-MacBook-Pro.local>
2018-10-09 07:17:54 -0400

    All the way through https://github.com/aws-amplify/docs/blob/master/js/tutorials/building-ionic-4-apps/index.md

--- todo-example/src/app/pages/list/list.page.ts
+++ todo-example/src/app/pages/list/list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController, Events } from '@ionic/angular';
+import { AmplifyService } from 'aws-amplify-angular'
 import { ListItemModal } from './list.item.modal';
 import { ToDoItem, ToDoList } from '../../classes/item.class';
 
@@ -10,47 +11,53 @@ import { ToDoItem, ToDoList } from '../../classes/item.class';
 })
 export class ListPage implements OnInit {
 
+  amplifyService: AmplifyService;
   modal: any;
   data: any;
   user: any;
-  itemList: ToDoList;
+  itemList: ToDoList|any;
   signedIn: boolean;
 
   constructor(
     public modalController: ModalController,
+    amplify: AmplifyService,
     events: Events
-
   ) {
     
+    this.amplifyService = amplify;
     // Listen for changes to the AuthState in order to change item list appropriately
     events.subscribe('data:AuthState', async (data) => {
       if (data.loggedIn){
+        this.user = await this.amplifyService.auth().currentUserInfo();
         this.getItems();
       } else {
-        // this.itemList.items = [];
+        this.itemList = [];
+        this.user = null;
       }
     })
   }
 
   async ngOnInit(){
+    // Use AWS Amplify to get user data when creating items
+    this.user = await this.amplifyService.auth().currentUserInfo();
     this.getItems();
   }
 
   async modify(item, i) {
     let props = {
-      itemList: this.itemList,
+      itemList: this.itemList || new ToDoList({userId: this.user.id}),
       /*
-        We pass in an item parameter only when the user clicks on an existing item
-        and therefore populate an editItem value so that our modal knows this is an edit operation.
+        You pass in an item parameter only when the user clicks on an existing item and therefore populate an editItem value so that your modal knows this is an edit operation.
       */
       editItem: item || undefined
     };
 
     // Create the modal
     this.modal = await this.modalController.create({
-       component: ListItemModal,
-       componentProps: props
+      component: ListItemModal,
+      componentProps: props
     });
+
     // Listen for the modal to be closed...
     this.modal.onDidDismiss((result) => {
       if (result.data.newItem){
@@ -67,36 +74,41 @@ export class ListPage implements OnInit {
 
   delete(i){
     this.itemList.items.splice(i, 1);
-    // this.save(this.itemList);
+    this.save(this.itemList);
   }
 
   complete(i){
     this.itemList.items[i].status = "complete";
-    // this.save(this.itemList);
+    this.save(this.itemList);
   }
 
   save(list){
     // Use AWS Amplify to save the list...
-    // this.itemList = list;
+    this.amplifyService.api().post('ToDoItemsCRUD', '/ToDoItems', {body: list}).then((i) => {
+      // ... and to get the list after you save it.
+      this.getItems()
+    })
+    .catch((err) => {
+      console.log(`Error saving list: ${err}`)
+    })
   }
 
   getItems(){
-    this.itemList = {
-      userId: 1,
-      items: [
-        new ToDoItem({
-          id: '1',
-          title: 'test item 1',
-          description: 'my test item',
-          status: 'complete'
-        }),
-        new ToDoItem({
-          id: '2',
-          title: 'test item 3',
-          description: 'my other test item',
-          status: 'pending'
-        })
-      ]
+    if (this.user){
+      // Use AWS Amplify to get the list
+      this.amplifyService.api().get('ToDoItemsCRUD', `/ToDoItems/${this.user.id}`, {}).then((res) => {
+        if (res && res.length > 0){
+          this.itemList = res[0];
+        } else {
+          this.itemList = new ToDoList({userId: this.user.id, items: []});
+        }
+      })
+      .catch((err) => {
+        console.log(`Error getting list: ${err}`)
+      })
+    } else {
+      console.log('Cannot get items: no active user')
     }
   }
+
 }
